1d9bb6a7526421be78fc15fb691897f86bebe657
Mike Rae <mikerae@me.com>
2022-07-26 18:22:03 +0000

    Get player name with greeting

--- run.py
+++ run.py
@@ -2,3 +2,13 @@
 Connect4 game for player v. computer
 """
 
+PLAYER = ""
+
+print("\n********************")
+print(" Welcome to Connect4")
+print("********************\n")
+
+while PLAYER.isspace() or PLAYER == "":
+    PLAYER = input("Please tell me your name...\n")
+
+print(f'Hello {PLAYER} , Lets play Connect4\n')
