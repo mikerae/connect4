d1893c81c9ba456d10fca20156cec935b12a0748
Mike Rae <mikerae@me.com>
2022-07-29 16:46:29 +0000

    Add turntaking

--- run.py
+++ run.py
@@ -37,15 +37,23 @@ def display_board(board):
     print(dec_row)
 
 
-def make_move(board, columns):
-    """ Players take turns to make thier move"""
+def player_move(board, columns, player_XO, name):
+    """ Player makes a move"""
     display_board(board)
+    print(f'{name} makes a move')
+    input("Choose column 1 - 7: ")
+
+
+def computer_move(board, columns, computer_XO):
+    """ Computer makes a move """
+    print("Computer makes a move")
+    input("Choose column 1 - 7: ")
 
 
 def check_win():
     """ Check if win conditions are met """
     print("check_win is called")
-    return True
+    return False
 
 
 def main():
@@ -54,34 +62,53 @@ def main():
     print(" Welcome to Connect4")
     print("********************\n")
 
-    player = ""
-    player_position = ""
+    name = ""
+    player_XO = ""
+    computer_XO = ""
     board = []
     columns = []
     board = build_empty_board(board)
     columns = build_empty_cols(columns)
 
-    while player.isspace() or player == "":
-        player = input("Please tell me your name...\n")
+    while name.isspace() or name == "":
+        name = input("Please tell me your name...\n")
 
-    print(f'\nHello {player}')
+    print(f'\nHello {name}')
 
     not_valid = True
     while not_valid:
-        player_position = input("Type X to play first, or O to play second: ")
-        if (player_position.isspace() or player_position == ""):
+        player_XO = input("Type X to play first, or O to play second: ")
+        if (player_XO.isspace() or player_XO == ""):
             continue
-        elif player_position.upper() == "X":
-            print(f'\nYou go first {player}, Make your move...\n')
+        elif player_XO.upper() == "X":
+            print(f'\nYou go first {name}, Make your move...\n')
+            player_XO = "X"
+            computer_XO = "O"
+            PLAYER = 0
+            COMPUTER = 1
             not_valid = False
-        elif player_position.upper() == "O":
-            print(f"\nI'll  go first then, {player}...\n")
+        elif player_XO.upper() == "O":
+            print(f"\nI'll  go first then, {name}...\n")
+            player_XO = "O"
+            computer_XO = "X"
+            PLAYER = 1
+            COMPUTER = 0
             not_valid = False
+
     win = False
+    turn = 0
     # Main game loop
     while not win:
-        make_move(board, columns)
-        win = check_win()
+        if turn == PLAYER:
+            # Player Move
+            player_move(board, columns, player_XO, name)
+            win = check_win()
+        elif turn == COMPUTER:
+            # Computer Move
+            computer_move(board, columns, computer_XO)
+            win = check_win()
+        turn += 1
+        turn = turn % 2
 
 
 main()
