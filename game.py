aeed8f92476dc518b4ba09257fbce13fb407aad1
Mike Rae <mikerae@me.com>
2022-08-06 12:23:18 +0000

    Refactor: move func to game.py

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ The game Class provides the functionality to store game data in the cloud
 via google worksheets.
 """
 from datetime import datetime
+from random import randint
 from slugify import slugify
 
 
@@ -24,6 +25,19 @@ class Game:
         print(f'To win, join four {self.xo}s:\n\
 horizontaly, verticaly or diagonaly.\n\
 Good Luck {self.player}!')
+
+    def process_win(self, winner, PLAYER, draw):
+        """ Displays winner message """
+        if draw:
+            print(f'Its a draw! Nice game {self.player}.')
+            self.winner = "draw"
+        elif PLAYER == winner:
+            print(f'{self.player}, you have won!\n')
+            self.winner = self.player
+        else:
+            print('I won this time!')
+            print(f'Better luck next time {self.player}!\n')
+            self.winner = "Computer"
 
     def final_update(self, board):
         """ Add final data in string form to game object:
@@ -51,3 +65,123 @@ Good Luck {self.player}!')
         print(f'The game ended at {self._end}')
         print(f'The game lasted {self._duration}')
         print(f'The raw board data is {self.board}')
+
+
+def build_empty_board(board):
+    """ Initialises empty board"""
+    for row in range(6):
+        row = ["-"] * 7
+        board.append(row)
+    return board
+
+
+def display_board(board):
+    """ Displays the current state of the board """
+    dec_row = ("----" * 7) + "-\n"
+    col_nums = "  1   2   3   4   5   6   7"
+    board_row = ""
+    print(dec_row)
+    print(col_nums)
+    for row in board:
+        for cell in row:
+            board_row += f'| {cell} '
+        board_row += "|\n"
+    print(board_row)
+    print(dec_row)
+
+
+def check_player_input(column, columns):
+    """
+    Checks if white space or "" is input
+    Checks chosen column is string in range 1 - 7
+    Checks if chosen column is full
+    """
+    try:
+        if column == "" or column.isspace():
+            raise ValueError(
+                'Please choose a column between 1 and 7'
+            )
+        elif int(column) not in range(1, 8):
+            raise ValueError(
+                'Please choose a column between 1 and 7'
+            )
+        elif len(columns[int(column) - 1]) >= 6:
+            raise ValueError(
+                f'Column {column} is full, please choose another column'
+            )
+        else:
+            return True
+    except ValueError as e:
+        print(f'{e}')
+        return False
+
+
+def player_move(board, columns, player_xo, column_full):
+    """ Player makes a move"""
+    display_board(board)
+    column = (input("Choose column 1 - 7: "))
+    if check_player_input(column, columns):
+        columns[int(column) - 1].push(player_xo)
+        board[6 - len(columns[int(column) - 1])][int(column) - 1] = \
+            columns[int(column) - 1].peek()
+        if len(columns[int(column) - 1]) >= 6:
+            column_full.append(True)
+    else:
+        player_move(board, columns, player_xo, column_full)
+    return board, columns, column_full
+
+
+def computer_move(board, columns, computer_xo, column_full):
+    """ Computer makes a move """
+    col = randint(0, 6)
+    if len(columns[col]) < 6:
+        columns[col].push(computer_xo)
+        board[6 - len(columns[col])][col] = \
+            columns[col].peek()
+        if len(columns[col]) >= 6:
+            column_full.append(True)
+    else:
+        computer_move(board, columns, computer_xo, column_full)
+    return board, columns, column_full
+
+
+def check_win(board, xo, turn):
+    """ Check if win conditions are met """
+    result = False
+    # Horizonatal check
+    for row in range(6):
+        for col in range(4):
+            if board[5 - row][col] == board[5 - row][col + 1] ==\
+                    board[5 - row][col + 2] == board[5 - row][col + 3] == xo:
+                result = True
+                break
+    # Verticle check
+    for col in range(7):
+        for row in range(4):
+            if board[5 - row][col] == board[5 - (row + 1)][col] ==\
+                    board[5 - (row + 2)][col] ==\
+                    board[5 - (row + 3)][col] == xo:
+                result = True
+                break
+    # +ve slope Diagonal or -ve slope Diagonal check
+    for row in range(6):
+        for col in range(4):
+            if board[5 - row][col] ==\
+                    board[5 - (row + 1)][col + 1] ==\
+                    board[5 - (row + 2)][col + 2] ==\
+                    board[5 - (row + 3)][col + 3] == xo or\
+                    board[5 - row][col + 3] ==\
+                    board[5 - (row + 1)][col + 2] ==\
+                    board[5 - (row + 2)][col + 1] ==\
+                    board[5 - (row + 3)][col] == xo:
+                result = True
+                break
+    return result, turn
+
+
+def check_draw(column_full):
+    """ Check for draw """
+    draw = False
+    if len(column_full) >= 7:
+        draw = True
+    return draw
--- run.py
+++ run.py
@@ -3,17 +3,9 @@ Connect4 game for player v. computer
 Mike Rae
 August 2022
 """
-from random import randint
 from stacks import Stack
-from game import Game
-
-
-def build_empty_board(board):
-    """ Initialises empty board"""
-    for row in range(6):
-        row = ["-"] * 7
-        board.append(row)
-    return board
+from game import Game, build_empty_board, display_board,\
+     player_move, computer_move, check_win, check_draw
 
 
 def build_empty_cols(columns):
@@ -22,118 +14,6 @@ def build_empty_cols(columns):
         col = Stack()
         columns.append(col)
     return columns
-
-
-def display_board(board):
-    """ Displays the current state of the board """
-    dec_row = ("----" * 7) + "-\n"
-    col_nums = "  1   2   3   4   5   6   7"
-    board_row = ""
-    print(dec_row)
-    print(col_nums)
-    for row in board:
-        for cell in row:
-            board_row += f'| {cell} '
-        board_row += "|\n"
-    print(board_row)
-    print(dec_row)
-
-
-def check_player_input(column, columns):
-    """
-    Checks if white space or "" is input
-    Checks chosen column is string in range 1 - 7
-    Checks if chosen column is full
-    """
-    try:
-        if column == "" or column.isspace():
-            raise ValueError(
-                'Please choose a column between 1 and 7'
-            )
-        elif int(column) not in range(1, 8):
-            raise ValueError(
-                'Please choose a column between 1 and 7'
-            )
-        elif len(columns[int(column) - 1]) >= 6:
-            raise ValueError(
-                f'Column {column} is full, please choose another column'
-            )
-        else:
-            return True
-    except ValueError as e:
-        print(f'{e}')
-        return False
-
-
-def player_move(board, columns, player_xo, column_full):
-    """ Player makes a move"""
-    display_board(board)
-    column = (input("Choose column 1 - 7: "))
-    if check_player_input(column, columns):
-        columns[int(column) - 1].push(player_xo)
-        board[6 - len(columns[int(column) - 1])][int(column) - 1] = \
-            columns[int(column) - 1].peek()
-        if len(columns[int(column) - 1]) >= 6:
-            column_full.append(True)
-    else:
-        player_move(board, columns, player_xo, column_full)
-    return board, columns, column_full
-
-
-def computer_move(board, columns, computer_xo, column_full):
-    """ Computer makes a move """
-    col = randint(0, 6)
-    if len(columns[col]) < 6:
-        columns[col].push(computer_xo)
-        board[6 - len(columns[col])][col] = \
-            columns[col].peek()
-        if len(columns[col]) >= 6:
-            column_full.append(True)
-    else:
-        computer_move(board, columns, computer_xo, column_full)
-    return board, columns, column_full
-
-
-def check_win(board, xo, turn):
-    """ Check if win conditions are met """
-    result = False
-    # Horizonatal check
-    for row in range(6):
-        for col in range(4):
-            if board[5 - row][col] == board[5 - row][col + 1] ==\
-                    board[5 - row][col + 2] == board[5 - row][col + 3] == xo:
-                result = True
-                break
-    # Verticle check
-    for col in range(7):
-        for row in range(4):
-            if board[5 - row][col] == board[5 - (row + 1)][col] ==\
-                    board[5 - (row + 2)][col] ==\
-                    board[5 - (row + 3)][col] == xo:
-                result = True
-                break
-    # +ve slope Diagonal or -ve slope Diagonal check
-    for row in range(6):
-        for col in range(4):
-            if board[5 - row][col] ==\
-                    board[5 - (row + 1)][col + 1] ==\
-                    board[5 - (row + 2)][col + 2] ==\
-                    board[5 - (row + 3)][col + 3] == xo or\
-                    board[5 - row][col + 3] ==\
-                    board[5 - (row + 1)][col + 2] ==\
-                    board[5 - (row + 2)][col + 1] ==\
-                    board[5 - (row + 3)][col] == xo:
-                result = True
-                break
-    return result, turn
-
-
-def check_draw(column_full):
-    """ Check for draw """
-    draw = False
-    if len(column_full) >= 7:
-        draw = True
-    return draw
 
 
 def start_again(name):
@@ -156,20 +36,6 @@ def start_again(name):
         main()
     else:
         print(f'Thanks for playing, {name}\nSee you again soon!\n')
-
-
-def process_win(winner, PLAYER, name, draw, game):
-    """ Displays winner message """
-    if draw:
-        print(f'Its a draw! Nice game {name}.')
-        game.winner = "draw"
-    elif PLAYER == winner:
-        print(f'{name}, you have won!\n')
-        game.winner = name
-    else:
-        print('I won this time!')
-        print(f'Better luck next time {name}!\n')
-        game.winner = "Computer"
 
 
 def main():
@@ -238,7 +104,7 @@ def main():
         if draw:
             break
     display_board(board)
-    process_win(winner, PLAYER, name, draw, game)
+    game.process_win(winner, PLAYER, draw)
     game.final_update(board)
     game.display_game_data()
     start_again(name)
