9e4937d08ced5c7d6867465551305833c58825cc
Mike Rae <mikerae@me.com>
2022-08-07 17:51:17 +0000

    Refactor display_games_list()

--- utils.py
+++ utils.py
@@ -15,7 +15,5 @@ def get_played_games_list(games_ss):
 def display_games_list(list):
     """ Displays a list of played games
     with index """
-    list_len = len(list)
-    i = 0
     for idl, l in enumerate(list):
         print(f'type {idl} to select {l}')
